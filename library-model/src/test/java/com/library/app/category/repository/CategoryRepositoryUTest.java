609e2138a7a7d96620ff735166592e4482a0253f
Laercio Metzner <laercio.metzner@gmail.com>
2017-11-20 01:02:10 -0200

    Lesson 17, added DBCommand and DBCommandTransactionalExecutor that porivides a convenient interface for developing tests that uses DB transactions 'the java8 way'

--- library-model/src/test/java/com/library/app/category/repository/CategoryRepositoryUTest.java
+++ library-model/src/test/java/com/library/app/category/repository/CategoryRepositoryUTest.java
@@ -13,36 +13,36 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.library.app.category.model.Category;
+import com.library.app.commontests.utils.DBCommand;
+import com.library.app.commontests.utils.DBCommandTransactionalExecutor;
 
 public class CategoryRepositoryUTest {
 
 	private EntityManagerFactory emf;
 	private EntityManager em;
 	private CategoryRepository categoryRepository;
+	private DBCommandTransactionalExecutor dBCommandTransactionalExecutor;
 
 	@Before
 	public void initTestCase() {
 
 		emf = Persistence.createEntityManagerFactory("libraryPU");
 		em = emf.createEntityManager();
-
 		categoryRepository = new CategoryRepository();
 		categoryRepository.em = em;
+		dBCommandTransactionalExecutor = new DBCommandTransactionalExecutor(em);
+
 	}
 
 	@Test
 	public void addCategoryAndFindIt() {
 		Long categoryAddedId = null;
 		try {
-			em.getTransaction().begin();
-			categoryAddedId = categoryRepository.add(java()).getId();
+			categoryAddedId = dBCommandTransactionalExecutor.executeCommand(() -> categoryRepository.add(java()).getId());
 			assertThat(categoryAddedId, is(notNullValue()));
-			em.getTransaction().commit();
-			em.clear();
 		} catch (final Exception e) {
 			fail("This exception should not have been thrown");
 			e.printStackTrace();
-			em.close();
 		}
 
 		final Category category = categoryRepository.findById(categoryAddedId);
