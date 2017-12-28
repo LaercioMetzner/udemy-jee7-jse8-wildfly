87fbf7250b02c8a138084112680283bdbf357881
Laercio Metzner (GOVBR - DDP) <Laercio.Metzner@cetil.com.br>
2017-12-28 13:19:38 -0200

    Just Started Implementing tests

--- library-resource/src/main/java/com/library/app/category/resource/CategoryResource.java
+++ library-resource/src/main/java/com/library/app/category/resource/CategoryResource.java
@@ -1,5 +1,10 @@
 package com.library.app.category.resource;
 
+import com.library.app.category.services.CategoryServices;
+
 public class CategoryResource {
+	
+	CategoryServices categoryServices;
+	
 
 }
--- library-resource/src/test/java/com/library/app/category/resource/CategoryResourceUTest.java
+++ library-resource/src/test/java/com/library/app/category/resource/CategoryResourceUTest.java
@@ -1,5 +1,25 @@
 package com.library.app.category.resource;
 
+import org.junit.Before;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.library.app.category.services.CategoryServices;
+
 public class CategoryResourceUTest {
 
+	private CategoryResource categoryResource;
+	
+	@Mock
+	private CategoryServices categoryServices;
+	
+	@Before
+	public void initTestCase() {
+		
+		MockitoAnnotations.initMocks(this);
+		
+		categoryResource = new CategoryResource();
+		categoryResource.categoryServices = categoryServices;
+	}
+	
 }
